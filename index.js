8c23df00911cd42ff211fa016a41e2c0be74dd65
unknown <icto@ICTOP.uva.nl>
2014-06-20 11:12:33 +0200

    holy fuck it's on nodejitsu and it WORKS

--- index.js
+++ index.js
@@ -3,19 +3,22 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendfile('index.html');
+	res.sendfile('index.html');
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-  });
+	console.log('a user connected');
+	socket.on('disconnect', function(){
+		console.log('user disconnected');
+	});
+	socket.on('chat message', function(msg){
+		console.log('message: ' + msg);
+		io.emit('chat message', msg);
+	});
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+var port = Number(process.env.PORT || 5000);
+
+http.listen(port, function(){
+	console.log('listening on *:' + port);
 });
