b2ae9b9fc90c51fb149e1e0f7645315f7304c4d9
unknown <icto@ICTOP.uva.nl>
2014-06-24 13:21:15 +0200

    population data correctly passes to server

--- index.js
+++ index.js
@@ -13,9 +13,16 @@ app.get('/', function(req, res){
 	res.sendfile('public/index.html');
 });
 
+var population = new Object();
+
 io.on('connection', function(socket){
 	console.log('a user connected');
-	console.log(socket.id);
+	var userId = socket.id;
+	var newUser = { 'uuid' : userId };
+	socket.emit('uuid', newUser);
+	population[userId] = new Object();
+	console.log(population);
+	// console.log(socket.id);
 // 	socket.on('disconnect', function(){
 // 		console.log('user disconnected');
 // 	});
@@ -23,6 +30,24 @@ io.on('connection', function(socket){
 // 		console.log('message: ' + msg);
 // 		io.emit('chat message', msg);
 // 	});
+
+	socket.on('newChar', function(e){
+		population[userId] = {
+			rank: e.rank,
+			file: e.file,
+			chr: e.chr,
+			clr: e.clr,
+			name: e.name,
+			lastknownplace: e.lastknownplace
+		};
+		// socket.emit('newCharAdded', population[userId]);
+		console.log(population);
+	});
+
+	socket.on('disconnect', function(){
+		delete population[userId];
+		console.log(population);
+	});
 });
 
 var port = Number(process.env.PORT || 5000);
@@ -30,3 +55,4 @@ var port = Number(process.env.PORT || 5000);
 http.listen(port, function(){
 	console.log('listening on *:' + port);
 });
+
