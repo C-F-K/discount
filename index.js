68510494f56ab25dcb769accf36899f72013a221
C-F-K <a.fell.ninny.mocker@gmail.com>
2014-07-16 02:58:30 +0200

    console is pretty much working yo

--- index.js
+++ index.js
@@ -16,6 +16,15 @@ app.get('/', function(req, res){
 var population = new Object();
 var occupiedSpaces = new Object();
 
+var commands = {
+	'me': function(prm) {
+		var fid = prm[0];
+		var action = prm[1];
+		var resString = population[fid].nm + " " + action;
+		return ['me', resString];
+	}
+};
+
 io.sockets.on('connection', function(socket){
 	// console.log('a user connected');
 	var userId = socket.id;
@@ -36,7 +45,7 @@ io.sockets.on('connection', function(socket){
 		};
 
 		var newSpace = e.file + e.rank;
-		occupiedSpaces[newSpace] = userId;
+		occupiedSpaces[newSpace] = population[userId].nm;
 		// console.log('occupied:');
 		// console.log(occupiedSpaces);
 		// console.log('user has specified char:');
@@ -59,7 +68,7 @@ io.sockets.on('connection', function(socket){
 		io.sockets.emit('movedUser', resPayload);
 	});
 	
-	socket.on('consoleCommand', function(payload){
+	socket.on('consoleMessage', function(payload){
 		var cid = payload[0];
 		var ccmd = payload[1];
 		var cnm = population[cid].nm;
@@ -68,9 +77,18 @@ io.sockets.on('connection', function(socket){
 		// if logging: add code here to add string to log
 	});
 
+	socket.on('consoleCommand', function(payload){
+		var cmd = payload.pop();
+		if (commands[cmd]) {
+			var returned = commands[cmd](payload);
+			io.sockets.emit('consoleReturn', returned);
+		}
+	});
+
 	socket.on('requestCharData', function(payload){
 
 	});
+
 	socket.on('updateCharData', function(payload){
 
 	});
@@ -92,4 +110,3 @@ var port = Number(process.env.PORT || 5000);
 http.listen(port, function(){
 	console.log('listening on *:' + port);
 });
-
